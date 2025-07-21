af1e6823d9f2794fead7359fce809c78dcb59c5c
Rogergabs <rogergbd2001@gmail.com>
2025-07-21 19:50:55 -0300

    Update PessoaFisica.java

--- loja/model/cliente/PessoaFisica.java
+++ loja/model/cliente/PessoaFisica.java
@@ -1,5 +1,19 @@
+// PessoaFisica.java
 package loja.model.cliente;
 
-public class PessoaFisica {
-    
+public class PessoaFisica extends Cliente {
+    private String cpf;
+
+    public PessoaFisica(String identificador, String nome, String endereco, String telefone, String cpf) {
+        super(identificador, nome, endereco, telefone);
+        this.cpf = cpf;
+    }
+
+    // Getter específico
+    public String getCpf() { return cpf; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - CPF: " + cpf;
+    }
 }
