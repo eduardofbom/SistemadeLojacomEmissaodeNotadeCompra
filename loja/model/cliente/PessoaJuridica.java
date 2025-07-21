6678373364bbb3ae70388daebf252a7c5ddbfc8d
Rogergabs <rogergbd2001@gmail.com>
2025-07-21 20:44:11 -0300

    Update PessoaJuridica.java

--- loja/model/cliente/PessoaJuridica.java
+++ loja/model/cliente/PessoaJuridica.java
@@ -1,5 +1,21 @@
+// PessoaJuridica.java
 package loja.model.cliente;
 
-public class PessoaJuridica {
-    
+public class PessoaJuridica extends Cliente {
+    private String cnpj;
+    private String inscricaoEstadual;
+
+    public PessoaJuridica(String identificador, String nome, String endereco, String telefone, 
+                          String cnpj, String inscricaoEstadual) {
+        super(identificador, nome, endereco, telefone);
+        this.cnpj = cnpj;
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getCnpj() { return cnpj; }
+    public String getInscricaoEstadual() { return inscricaoEstadual; }
+
+    public String toString() {
+        return super.toString() + " - CNPJ: " + cnpj + " - IE: " + inscricaoEstadual;
+    }
 }
